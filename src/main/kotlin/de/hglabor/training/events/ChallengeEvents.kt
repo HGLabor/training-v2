09ee2189e5fbee467a8b3c22a09ad0500f080ca5
Krxwallo <ken.grill@gmx.de>
2021-07-11 12:52:41 +0200

    fix challenge changing when in spectator

--- src/main/kotlin/de/hglabor/training/events/ChallengeEvents.kt
+++ src/main/kotlin/de/hglabor/training/events/ChallengeEvents.kt
@@ -5,15 +5,15 @@ import de.hglabor.training.challenge.challenge
 import de.hglabor.training.challenge.challenges
 import de.hglabor.training.challenge.notInChallenge
 import de.hglabor.training.utils.extensions.inRegion
-import de.hglabor.training.utils.extensions.isCreative
 import net.axay.kspigot.event.listen
+import org.bukkit.GameMode
 import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerMoveEvent
 
 
 fun regionListener() {
     listen<PlayerMoveEvent> { event -> with(event) {
-        if (!(player.isCreative()) && to?.distanceSquared(from) != 0.0) updatePlayerChallenge(player)
+        if (player.gameMode == GameMode.SURVIVAL && to?.distanceSquared(from) != 0.0) updatePlayerChallenge(player)
     }}
 }
 
